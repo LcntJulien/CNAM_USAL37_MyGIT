da95c9243b46de7890be57122ad7556acbe730de
Julien <73828002+LcntJulien@users.noreply.github.com>
2021-01-07 14:59:52 +0100

    snapshot

--- USAL37/tp_freelencer/tp_freelancer_init.sql
+++ USAL37/tp_freelencer/tp_freelancer_init.sql
@@ -16,7 +16,7 @@ create table customer_cats
 (
 	cat_id int auto_increment,
     cat_name varchar(50) not null,
-    cat_description text not null,
+    cat_description text null,
     primary key (cat_id)
 );
 
@@ -25,8 +25,8 @@ create table jobs
 	job_id int auto_increment,
     job_state char(10) not null,
     job_title varchar(100) not null,
-    job_start datetime not null,
-    job_end datetime not null,
+    job_start datetime null,
+    job_end datetime null,
     job_description text not null,
     primary key (job_id)
 );
@@ -35,16 +35,16 @@ create table quotes
 (
 	quote_id int auto_increment,
     quote_state char(10) not null,
-    quote_date date not null,
+    quote_date date null,
     quote_amount int not null,
-    quote_final_date date not null,
+    quote_final_date date null,
     quote_final_amount int not null,
     primary key (quote_id)
 );
 
 alter table customers
 	add
-    constraint fk_customers_cats foreign key (cat_id) references customers_cats(cat_id);
+    constraint fk_customer_cats foreign key (cat_id) references customer_cats(cat_id);
     
 alter table jobs
 	add
